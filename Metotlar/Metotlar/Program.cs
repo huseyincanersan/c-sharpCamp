059181263cc45a360db7506c6ad1a21a64b07288
Hüseyin Can <62290639+huseyincanersan@users.noreply.github.com>
2022-06-20 02:41:25 +0300

    c-sharp metot

---
+++ Metotlar/Metotlar/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Product product1 = new Product();
+
+            product1.Name = "Elma";
+            product1.Price = 15;
+            product1.Explanation = "Amasya Elmasi";
+
+            Product product2 = new Product();
+            
+            product2.Name = "Cilek";
+            product2.Price = 12;
+            product2.Explanation = "Taze Cilek";
+
+            Product product3 = new Product();
+
+            product3.Name = "Karpuz";
+            product3.Price = 80;
+            product3.Explanation = "Diyarbakir karpuzu";
+
+            Product[] products = new Product[] {product1,product2,product3 };
+
+            foreach (var pro in products)
+            {
+                Console.WriteLine("Urun adi: " + pro.Name + " Urun Fiyati: "+ pro.Price + " Urun aciklamasi: " + pro.Explanation);
+            }
+
+            Console.WriteLine("---------Metotlar-----------");
+
+            SepetManager sepetManager = new SepetManager();
+
+            sepetManager.Addsepet(product1);
+            sepetManager.Addsepet(product2);
+            sepetManager.Addsepet(product3);
+
+
+        }
+    }
+}
---
+++ Metotlar/Metotlar/SepetManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlar
+{
+    class SepetManager
+    {
+        public void Addsepet(Product product)
+        {
+            Console.WriteLine("Sepete eklendi " + product.Explanation);
+        }
+        
+    }
+}
