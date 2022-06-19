6a8d6321583aead1812aadd9cdb72cecfa58afa2
Hüseyin Can <62290639+huseyincanersan@users.noreply.github.com>
2022-06-19 14:21:17 +0300

    Veri tipleri - Sart bloklari kullanimi

--- CampIntro/CampIntro/Program.cs
+++ CampIntro/CampIntro/Program.cs
@@ -6,7 +6,42 @@ namespace CampIntro
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //type safety = tip guvenligi
+            //Do not yourself - Kendini tekrarlama
+            string kategoriEtiketi = "Kategori";
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmismi = true;
+            double dolarDun = 7.45;
+            double dolarBugun = 7.45;
+
+            if (dolarDun >dolarBugun)
+            {
+                Console.WriteLine("Azalis Butonu");
+            }
+            else if(dolarDun < dolarBugun)
+            {
+                Console.WriteLine("Artis Butonu");
+            }
+            else
+            {
+                Console.WriteLine("Esittir Butonu");
+            }
+
+
+            if (sistemeGirisYapmismi == false)
+            {
+                Console.WriteLine("Kayit ol");
+            }
+            else
+            {
+                Console.WriteLine("Kullanici ayarlari");
+            }
+
+            Console.WriteLine(kategoriEtiketi);
+
+
+
         }
     }
 }
