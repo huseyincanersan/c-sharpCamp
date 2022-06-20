569fb7102b26db0c9cba295a254db9106cec8be4
Hüseyin Can <62290639+huseyincanersan@users.noreply.github.com>
2022-06-20 03:13:36 +0300

    referans tipler ve deger tipler arasindaki fark\

---
+++ Metotlar/DegerVeReferansTipler/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace DegerVeReferansTipler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //deger tipler int - string - bool - decimal
+            // deger tipler stack kisminda depolanir
+            int sayi1 = 10;
+            int sayi2 = 30;
+            sayi1 = sayi2;
+            sayi2 = 65;
+
+            //sayi1 ????
+            //Console.WriteLine(sayi1);
+
+
+            //referans tipler array-class-interface
+            //referans tipler stack kisminda tanimlanir heap kisminda degerlenir
+
+            int[] sayilar1 = new int[] {10,20,30 };
+            int[] sayilar2 = new int[] { 100, 500, 1000 };
+            sayilar1 = sayilar2;
+            sayilar2[0] = 999;
+
+            //sayilar1[0] ????
+
+            //Console.WriteLine(sayilar1[0]);
+
+
+        }
+    }
+}
