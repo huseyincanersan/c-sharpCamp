dedbaa6787757224191750d1424ae8c03c9b7edc
Hüseyin Can <62290639+huseyincanersan@users.noreply.github.com>
2022-06-21 14:43:11 +0300

    oop1

---
+++ OOP1/Collections/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] isimler = new string[] { "Can", "Huseyin", "Halil", "Ibrahim" };
+            //Console.WriteLine(isimler[0]);
+            //Console.WriteLine(isimler[1]);
+            //Console.WriteLine(isimler[2]);
+            //Console.WriteLine(isimler[3]);
+
+            //isimler = new string[5];
+            //isimler[4] = "Ilker";
+            //Console.WriteLine(isimler[4]);
+            //Console.WriteLine(isimler[0]);
+
+            List<string> isimler2 = new List<string> {"can","Huseyin","Halil" };
+
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+
+            isimler2.Add("Ilker");
+
+            Console.WriteLine(isimler2[3]);
+
+            
+
+
+
+        }
+    }
+}
