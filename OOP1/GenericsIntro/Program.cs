038191abdfcab0d9bae55e6bef5536e677b868d4
Hüseyin Can <62290639+huseyincanersan@users.noreply.github.com>
2022-06-22 14:01:14 +0300

    collections eklendi

---
+++ OOP1/GenericsIntro/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> isimler = new MyList<string>();
+
+           
+           isimler.Add("Can");
+
+            Console.WriteLine(isimler.Length);
+
+            isimler.Add("Huseyin");
+
+            Console.WriteLine(isimler.Length);
+
+            foreach (var item in isimler.Items)
+            {
+                Console.WriteLine(item);
+            }
+
+
+        }
+    }
+}
