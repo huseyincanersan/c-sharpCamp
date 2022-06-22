013dc9d175a4fc8889e02f0807c1ed67f3b9b1f7
Hüseyin Can <62290639+huseyincanersan@users.noreply.github.com>
2022-06-22 15:45:22 +0300

    constructor yapisi

---
+++ Constructors/Constructors/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer {Id = 1 ,FirstName="Can",LastName="Ersan",City="istanbul" };
+
+            Customer customer2 = new Customer(2,"huseyin","ersan","istanbul");
+
+            Console.WriteLine(customer2.FirstName);
+        }
+    }
+
+    class Customer
+    {
+        //default constructor
+        public Customer()
+        {
+            Console.WriteLine("Yapici Blok Calisti");
+        }
+
+        public Customer(int id, string firsName, string lastName,string city)
+        {
+            Id = id;
+            FirstName = firsName;
+            LastName = lastName;
+            City = city;
+        }
+
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string City { get; set; }
+    }
+}
