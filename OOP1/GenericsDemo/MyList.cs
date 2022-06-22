b05faead0593108eebac4d94e2e39288027126bf
Hüseyin Can <62290639+huseyincanersan@users.noreply.github.com>
2022-06-22 17:00:38 +0300

    generic demo eklendi

---
+++ OOP1/GenericsDemo/MyList.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsDemo
+{
+    class MyList<A>
+    {
+        A[] items;
+
+        public MyList()
+        {
+            items = new A[0];
+        }
+
+
+        public void Add(A item)
+        {
+            A[] tempArray = items;
+
+            items = new A[items.Length + 1];
+
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+
+            items[items.Length - 1] = item;
+
+        }
+
+        public int Count
+        {
+            get { return items.Length; }
+        }
+
+        public A[] Items
+        {
+            get { return items;}
+        }
+        
+    }
+}
---
+++ OOP1/GenericsDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace GenericsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> sehirler = new MyList<string>();
+
+            sehirler.Add("Ankara");
+            Console.WriteLine(sehirler.Count);
+            sehirler.Add("Istanbul");
+
+            Console.WriteLine(sehirler.Count);
+
+            foreach (var item in sehirler.Items)
+            {
+                Console.WriteLine(item);
+            }
+
+
+
+
+
+
+        }
+    }
+}
