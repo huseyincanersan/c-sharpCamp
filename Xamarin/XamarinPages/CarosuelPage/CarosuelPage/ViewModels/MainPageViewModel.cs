90c1ed90ce3a0bcdbeea22221ac34baa96bb690d
Hüseyin Can <62290639+huseyincanersan@users.noreply.github.com>
2022-07-13 16:30:32 +0300

    xamarin carousel page

---
+++ Xamarin/XamarinPages/CarosuelPage/CarosuelPage/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using Xamarin.Forms;
+using CarosuelPage.Models;
+using CarosuelPage.Services;
+using System.ComponentModel;
+
+namespace CarosuelPage.ViewModels
+{
+
+
+
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<Car> carlist;
+        Car selectedItem;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ObservableCollection<Car> Carlist { get => carlist; set => carlist = value; }
+        public Car SelectedItem
+        {
+            get => selectedItem; set
+            {
+                if (selectedItem != value)
+                {
+                    selectedItem = value;
+                    OnPropertyChanged("SelectedItem");
+                }
+            }
+        }
+
+        public MainPageViewModel()
+        {
+            Carlist = ListCreator.getCars();
+        }
+
+        public virtual void OnPropertyChanged(string propertName)
+        {
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertName));
+        }
+    }
+}
